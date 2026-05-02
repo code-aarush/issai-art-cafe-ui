4acfa038057db94d54bb7a954215b6d510c51ef6
Aarush Sivaraman <aarush.sivaraman@gmail.com>
2026-05-02 15:38:01 +0530

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-function f() {
-    console.log("Hello, World!");
-}
-=======
-document.getElementById("demo").innerHTML = "My commit !!!"
->>>>>>> 817f342d2f9ca742e81e451e719ba38c2915392b
